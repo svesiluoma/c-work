290eea6d7ae837d340c461b899c0b9d6a7d12f3c
Sari Vesiluoma <svesiluoma@hotmail.com>
2021-03-16 20:55:31 +0200

    3 ja 4 kesken

---
+++ large.c
@@ -0,0 +1,144 @@
+/*
+ * large.c
+ * 3-large, suurempien tietomäärien käsittely pistokkeessa
+ * Sari Vesiluoma */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "template.h"
+
+#define MAXLINE 80
+
+int main(int argc, char **argv)
+{
+    int sockfd, n;
+    char recvline[MAXLINE + 1];  // Merkkipuskuri, johon luetaan tietoa
+    struct sockaddr_in servaddr; //tietorakenne, joka esittää osoitetta
+
+    // Ottaa TCP-yhteyden IP-osoitteeseen 130.233.154.208, porttiin 5000.
+    const char *address = "130.233.154.208";
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket error");
+        return 1;
+    } 
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(5000);
+
+    if (inet_pton(AF_INET, address, &servaddr.sin_addr) <= 0) {
+        fprintf(stderr, "inet_pton error for %s/n", address);
+        return 1;
+    }
+
+    if (connect(sockfd,
+                (struct sockaddr *) &servaddr,
+                sizeof(servaddr)) < 0) {
+                    perror("connect error");
+                    return 1;
+    }
+
+    // Lähettää pistokkeeseen oman opiskelijanumerosi ASCII-muodossa ja sen perään rivinvaihtomerkin
+    char *opnro = "763088\n";
+    n = write(sockfd, opnro, strlen(opnro));
+    if (n < 0) {
+        perror("write1 error\n");
+        return 1;
+    }
+
+    // Lähettää pistokkeeseen tehtävän tunnisteen ja perään rivinvaihdon
+    char *viesti = "3-large\n";
+    n = write(sockfd, viesti, strlen(viesti));
+    if (n < 0) {
+        perror("write2 error\n");
+        return 1;
+    }
+
+    // Tämän jälkeen palvelin alkaa lähettää sinulle tietolohkoja.
+    while (1) {
+        printf("Loopin kierros alkaa\n");
+        // Kukin tietolohko alkaa 32-bittisellä etumerkittömällä (unsigned) 
+        // kokonaisluvulla, joka kertoo kuinka monta tavua tietolohko 
+        // sisältää (muista verkon tavujärjestys!).
+        uint32_t item32 = 0;
+        n = read(sockfd, &item32, sizeof(uint32_t));
+        printf("n ekan luvun lukemisen jälkeen: %d\n", n);
+        if (n < 0) {
+            perror("read item32 error\n");
+            return 1;
+        }    
+        uint32_t result = ntohl(item32);
+        printf("Tavujen määrä: %d\n", result);
+        if (result == 0) {
+            printf("Nyt tulos on nolla: %d\n", result);
+            // Kun saat takaisin luvun nolla, lähetä takaisin luku 0 
+            uint32_t *buf, it, item32;
+            item32 = 0;
+            buf = &it;
+            it = htonl(item32);
+            n = write(sockfd, &it, sizeof(uint32_t));
+            if (n < 0) {
+                perror("write last 0 error\n");
+                return 1;
+            }         
+            break;
+        }
+
+        // Tämän jälkeen seuraa kyseinen tavumäärä, 
+        // joka sinun pitää lukea pistokkeesta.
+        char *returndata = malloc(result * sizeof(char));
+        //returndata = malloc(sizeof(char)*result);
+        // char returndata[result+1];
+        uint32_t i = 0;
+        while ((n = read(sockfd, returndata, sizeof(char)*(result))) > 0) {
+            printf("n tavuluvun jälkeen: %d\n", n);
+            i += n;
+            //if (fputs(returndata, stdout) == EOF) {
+            //    fprintf(stderr, "fputs error\n");
+            //    return 1;
+            //}
+        }
+        if (n < 0) {
+            perror("read error");
+            return 1;
+        }
+        free(returndata);
+        returndata = NULL;
+
+        printf("Ennen tavujen määrän lähettämistä i = %d\n", i);
+        // Kun olet lukenut koko lohkon, lähetä takaisin 
+        // palvelimelle lukemiesi tavujen määrä (eli lohkon koko), 
+        // jälleen 32-bittisenä kokonaislukuna, verkkotavujärjestyksessä.
+        uint32_t *buf, it;
+        buf = &it;
+        it = htonl(item32);
+        n = write(sockfd, &it, sizeof(uint32_t));
+        if (n < 0) {
+            perror("write it error\n");
+            return 1;
+        }  
+        printf("Tavujen määrä lähettämisen jälkeen: %d\n", n);  
+    }
+    printf("Loopin jälkeen\n");
+
+    // Viimeisenä serveri vastaa OK tai FAIL
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = 0;
+        if (fputs(recvline, stdout) == EOF) {
+            fprintf(stderr, "fputs error\n");
+            return 1;
+        }
+        printf("Viimeisessä viestissä luettu n = %d merkkiä\n", n);
+    }
+    if (n < 0) {
+        perror("read error");
+        return 1;
+    }
+    return 0;
+   
+    // Sulkee pistokkeen
+    close(sockfd);
+}
---
+++ names.c
@@ -0,0 +1,254 @@
+/*
+ * large.c
+ * 3-large, suurempien tietomäärien käsittely pistokkeessa
+ * Sari Vesiluoma */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "template.h"
+
+#define MAXLINE 80
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+
+// Apufunktio osoitteen tulostamiseen
+void print_address(const char *prefix, const struct addrinfo *res, char *ip_address)
+{
+        char outbuf[80];
+        struct sockaddr_in *sin = (struct sockaddr_in *)res->ai_addr;
+        struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)res->ai_addr;
+        void *address;
+
+        if (res->ai_family == AF_INET)
+                address = &(sin->sin_addr);
+        else if (res->ai_family == AF_INET6)
+                address = &(sin6->sin6_addr);
+        else {
+                printf("Unknown address\n");
+                return;
+        }
+
+        const char *ret = inet_ntop(res->ai_family, address, outbuf, sizeof(outbuf));
+        printf("%s %s\n", prefix, ret);
+        strcpy(ip_address, ret);
+}
+
+// host: kohdesolmun nimi (tai IP-osoite tekstimuodossa)
+// serv: kohdepalvelun nimi (tai porttinumero tekstimuodossa)
+int tcp_connect(const char *host, const char *serv, char *ip_address)
+{
+    int sockfd, n;
+    struct addrinfo hints, *res, *ressave;
+
+    // Ensiksi kerrotaan hints-rakenteessa, että osoiteperhettä ei ole
+    // rajoitettu, eli sekä IPv4 ja IPv6 - osoitteet kelpaavat.
+    // Lisäksi sanomme, että olemme pelkästään kiinnostuneita TCP-yhteyksistä
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    // Tehdään nimikysely käyttäen ylläolevaa hints-rakennetta
+    // Funktio varaa vastaukselle tilan itse, osoitin palautuu res-muuttujaan
+    if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0) {
+            fprintf(stderr, "tcp_connect error for %s, %s: %s\n",
+                    host, serv, gai_strerror(n));
+            return -1;
+    }
+    ressave = res; // so that we can release the memory afterwards
+
+    // res-rakenne osoittaa linkitettyyn listaan. Käydään läpi linkitettyä
+    // listaa yksi kerrallaan ja yritetään yhdistää saatuun osoitteeseen.
+    // res-rakenne sisältää kaikki parameterit mitä tarvitsemme socket-
+    // ja connect - kutsuissa.
+    do {
+           sockfd = socket(res->ai_family, res->ai_socktype,
+                            res->ai_protocol);
+            if (sockfd < 0)
+                    continue;       /* ignore this one */
+
+            print_address("Trying to connect", res, ip_address);
+
+            // Mikäli yhteys onnistuu, silmukka keskeytetään välittömästi,
+            // koska meillä on toimiva yhteys, eikä loppuja osoitteita
+            // tarvitse kokeilla
+            if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0)
+                  break;          /* success */
+
+            printf("connect failed\n");
+
+            close(sockfd);  /* ignore this one */
+    } while ( (res = res->ai_next) != NULL);
+
+    // Päästiinkö linkitetyn listan loppuun, mutta yhteys ei onnistunut?
+    // ==> virhe
+    if (res == NULL) {      /* errno set from final connect() */
+            fprintf(stderr, "tcp_connect error for %s, %s\n", host, serv);
+            sockfd = -1;
+    } else {
+            print_address("We are using address", res, ip_address);
+    }
+
+    // Järjestelmä on varannut muistin linkitetylle listalle, se pitää vapauttaa
+    freeaddrinfo(ressave);
+
+    return sockfd;
+}
+
+
+int main(int argc, char **argv)
+{
+    int sockfd, sockfd2, n;
+    char recvline[MAXLINE + 1];  // Merkkipuskuri, johon luetaan tietoa
+    struct sockaddr_in servaddr; //tietorakenne, joka esittää osoitetta
+
+    // Ottaa TCP-yhteyden IP-osoitteeseen 130.233.154.208, porttiin 5000.
+    const char *address = "130.233.154.208";
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket error");
+        return 1;
+    } 
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(5000);
+
+    if (inet_pton(AF_INET, address, &servaddr.sin_addr) <= 0) {
+        fprintf(stderr, "inet_pton error for %s/n", address);
+        return 1;
+    }
+
+    if (connect(sockfd,
+                (struct sockaddr *) &servaddr,
+                sizeof(servaddr)) < 0) {
+                    perror("connect error");
+                    return 1;
+    }
+
+    // Lähettää pistokkeeseen oman opiskelijanumerosi ASCII-muodossa ja sen perään rivinvaihtomerkin
+    char *opnro = "763088\n";
+    n = write(sockfd, opnro, strlen(opnro));
+    if (n < 0) {
+        perror("write1 error\n");
+        return 1;
+    }
+
+    // Lähettää pistokkeeseen tehtävän tunnisteen ja perään rivinvaihdon
+    char *viesti = "4-names\n";
+    n = write(sockfd, viesti, strlen(viesti));
+    if (n < 0) {
+        perror("write2 error\n");
+        return 1;
+    }
+
+    // Tämän jälkeen palvelin lähettää komennon merkkijonona, 
+    // jossa on DNS-nimi ja portti tässä formaatissa: CONN <DNS-nimi> <portti>. 
+    // Merkkijono loppuu rivinvaihtomerkkiin. 
+    // Esimerkiksi: CONN www.google.fi 80.
+    printf("Luetaan palvelimelta tuleva rivi\n");
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        if (fputs(recvline, stdout) == EOF) {
+            fprintf(stderr, "fputs error\n");
+            return 1;
+        }
+        if(strchr(recvline, '\n') != NULL) {
+            break;
+        }
+    }
+    if (n < 0) {
+        perror("read error");
+        return 1;
+    }
+    strtok (recvline,"\n");
+
+    // Luetaan osoite ja portti
+    char got_address[100];
+    char got_port[10];
+    sscanf(recvline, "CONN %s %s", got_address, got_port);
+    printf("Osoite on %s ja portti on %s\n", got_address, got_port);
+
+    // Tämän jälkeen sinun tulisi tehdä DNS-kysely 
+    // ja ottaa yhteys nimeä vastaavaan palvelimeen annettuun porttiin, 
+    // sulkematta kuitenkaan ensimmäistä pistoketta. 
+    // Luo siis toinen pistoke (pistoke-B), josta otat yhteyden.
+    char *ip_address=(char *)malloc(100);
+    int fd = tcp_connect(got_address, got_port, ip_address);
+    if (fd >= 0) {
+        printf("success!\n");
+    } else {
+        perror("fail: \n");
+    }
+
+    printf("ip-osoite on: %s\n", ip_address);
+    // Kun olet avannut yhteyden nimeä vastaavaan osoitteeseen, 
+    // lähetä oma IP-osoitteesi ja käyttämäsi portti 
+    // (jota käytät tässä toisessa pistokkeessa) pistoke-B:hen 
+    // palvelimelle formaatissa: ADDR <IP-osoite> <portti> <Opiskelijanro>, 
+    // jonka perään tulee rivinvaihtomerkki.  
+    //Tämän jälkeen pistoke-B:n palvelin sulkee yhteyden.
+    char *eka = "ADDR ";
+    n = write(fd, eka, strlen(eka));
+    if (n < 0) {
+        perror("write1 error\n");
+        return 1;
+    }
+    n = write(fd, ip_address, strlen(ip_address));
+    if (n < 0) {
+        perror("write2 error\n");
+        return 1;
+    }
+    n = write(fd, " ", sizeof(char));
+    if (n < 0) {
+        perror("write3 error\n");
+        return 1;
+    }
+    n = write(fd, got_port, strlen(got_port));
+    if (n < 0) {
+        perror("write4 error\n");
+        return 1;
+    }
+n = write(fd, " ", sizeof(char));
+    if (n < 0) {
+        perror("write5 error\n");
+        return 1;
+    }
+    n = write(fd, opnro, strlen(opnro));
+    if (n < 0) {
+        perror("write6 error\n");
+        return 1;
+    }
+    printf("Kirjoitettiin: %s%s %s %s", eka, ip_address, got_port, opnro);
+    // Jos yhteydenotto tähän osoitteeseen ei onnistu, 
+    // lähetä pistoke-A:han merkkijono FAIL, joka loppuu rivinvaihtomerkkiin. 
+    // Tämä tehtävä ei toimi NATin takaa. Miksi?
+
+
+    // Tämän jälkeen odota uutta komentoa palvelimelta (pistoke-A:sta), 
+    // kunnes pistoke-A:sta tulee joko “OK” tai “FAIL”.
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = 0;
+        if (fputs(recvline, stdout) == EOF) {
+            fprintf(stderr, "fputs error\n");
+            return 1;
+        }
+    }
+    if (n < 0) {
+        perror("read error");
+        return 1;
+    }
+    return 0;
+   
+    // Sulkee pistokkeen
+    close(sockfd);
+}
